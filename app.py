544ed360e8aa3b8ba7b526960b76fe72bb56e548
Fedor <ignatov.fedor.evgenivich@gmail.com>
2021-03-10 12:57:15 +0400

    add page book and about

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-"""Init site"""
+"""Create route URL"""
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world() -> str:
-    """Home page site"""
+    """Index page"""
     return "hellow"
+
+@app.route('/book/<int:book_id>')
+def show_book(book_id) -> int:
+    """Show book with given id"""
+    return 'Book %d' % book_id
+
+@app.route('/about/')
+def about()-> str:
+    """About information the site"""
+    return 'it\'s site Book shop'
+
+if __name__ == "__main__":
+    pass
